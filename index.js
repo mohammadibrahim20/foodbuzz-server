ed22eebcd232eb039379fab6f63d7c0d4ac0864e
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-05-03 16:49:19 +0600

    Update Photo URL

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const chefData = require("./data/chef.json");
+const chinesFood = require("./data/chiness.json");
 
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -10,6 +11,10 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send(chefData);
 });
+app.get("/chines", (req, res) => {
+  res.send(chinesFood);
+});
+
 app.get("/chef/:id", (req, res) => {
   const _id = req.params.id
   const singleData = chefData.find(d=> d.id == _id)
